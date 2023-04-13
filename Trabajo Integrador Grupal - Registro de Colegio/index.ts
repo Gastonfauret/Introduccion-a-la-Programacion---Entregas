5ac13d9ea5854f3486130a29bf71426fc0e43f8f
Gastón Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-04-13 12:26:01 -0300

    Funcion Promedio, Asignacion de Notas: Hecho

--- Trabajo Integrador Grupal - Registro de Colegio/index.ts
+++ Trabajo Integrador Grupal - Registro de Colegio/index.ts
@@ -2,16 +2,67 @@ import Alumnos from "./claseAlumnos";
 
 const datosAlumnos: Array<{}> = [];
 
-const modalidadSociales: {} = {Sociales: 0, Civica: 0, Politica: 0, Sociologia: 0};
-const modalidadNaturales: {} = {Biologia: 0, Fisica: 0, Quimica: 0, Anatomia: 0};
+const modalidadSociales = {sociales: 0, civica: 0, politica: 0, sociologia: 0};
+const modalidadNaturales = {biologia: 0, fisica: 0, quimica: 0, anatomia: 0};
 
-const juanPerez = new Alumnos('Juan', 'Perez', 42347925, '07/12/2010', 100, modalidadSociales,);
-datosAlumnos.push(juanPerez);
+console.log(modalidadSociales);
+console.log(modalidadNaturales);
 
-const marioGarcia = new Alumnos('Mario', 'Garcia', 40367121, '13/01/1009', 101, modalidadNaturales);
-datosAlumnos.push(marioGarcia);
+modalidadSociales.sociales = 9;
+modalidadSociales.civica = 8;
+modalidadSociales.politica = 7;
+modalidadSociales.sociologia = 10;
 
-const joseMartinez = new Alumnos('Jose', 'Martinez', 44789098, '12/04/2011', 102, modalidadNaturales);
-datosAlumnos.push(joseMartinez);
+modalidadNaturales.biologia = 7;
+modalidadNaturales.fisica = 6;
+modalidadNaturales.quimica = 1;
+modalidadNaturales.anatomia = 0;
 
-console.log(datosAlumnos);
+console.log(modalidadSociales);
+console.log(modalidadNaturales);
+
+console.log(promedio(modalidadSociales));
+console.log(promedio(modalidadNaturales));
+
+
+// const juanPerez = new Alumnos('Juan', 'Perez', 42347925, '07/12/2010', 100, modalidadSociales,);
+// datosAlumnos.push(juanPerez);
+
+// const marioGarcia = new Alumnos('Mario', 'Garcia', 40367121, '13/01/1009', 101, modalidadNaturales);
+// datosAlumnos.push(marioGarcia);
+
+// const joseMartinez = new Alumnos('Jose', 'Martinez', 44789098, '12/04/2011', 102, modalidadNaturales);
+// datosAlumnos.push(joseMartinez);
+
+//console.log(datosAlumnos);
+
+function promedio(object: {}) {
+    const arrayNotas: Array<number> = Object.values(object);
+    const sumaNotas: number = arrayNotas.reduce((total, actual)=> total + actual, 0)
+    const promedio = sumaNotas / arrayNotas.length
+    return promedio
+};
+
+
+// const items = ['Gaston', 'Pedro', 'Juan']
+// const seleccion = readLineSync.keyInSelect(items)
+// console.log(`Hola ${items[seleccion]}, como estas?`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
