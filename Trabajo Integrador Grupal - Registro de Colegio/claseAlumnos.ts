7913c303d096c8b9f3ac88f8f2ecda18e9c884b0
Gaston Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-04-16 00:32:54 -0300

    Menu, Carga y Listado de alumnos: Funcionando

--- Trabajo Integrador Grupal - Registro de Colegio/claseAlumnos.ts
+++ Trabajo Integrador Grupal - Registro de Colegio/claseAlumnos.ts
@@ -4,17 +4,19 @@ export default class Alumnos implements Personas {
     apellido: string;
     dni: number;
     fechaNacimiento: string;
-    matricula: number;    
+    matricula: number;
+    modalidad: string;   
     materias: {};
     promedio: number;
     profesores: {};
 
-    constructor(nombre: string, apellido: string, dni: number, fechaNacimiento: string, matricula: number, materias: {}, promedio: number, profesores: {}) {
+    constructor(nombre: string, apellido: string, dni: number, fechaNacimiento: string, matricula: number, modalidad: string, materias: {}, promedio: number, profesores: {}) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.fechaNacimiento = fechaNacimiento;
         this.matricula = matricula;
+        this.modalidad = modalidad;
         this.materias = materias;
         this.promedio = promedio;
         this.profesores = profesores;
--- Trabajo Integrador Grupal - Registro de Colegio/index.ts
+++ Trabajo Integrador Grupal - Registro de Colegio/index.ts
@@ -1,69 +1,8 @@
 import Alumnos from "./claseAlumnos";
-import promptSync from 'prompt-sync';
-const fs = require('fs');
+//const fs = require('fs');
 const readlineSync = require('readline-sync')
 
-//const prompt = promptSync();
-
 const datosAlumnos: Array<{}> = [];
-
-/*
-console.log('--------------------------------------------------');
-
-const modalidadSociales = {sociales: 0, civica: 0, politica: 0, sociologia: 0};
-
-const notaSociales: number = Number(prompt('Nota de Sociales: '));
-const notaCivica: number = Number(prompt('Nota de Civica: '));
-const notaPolitica: number = Number(prompt('Nota de Politica: '));
-const notaSociologia: number = Number(prompt('Nota de Sociologia: '));
-
-modalidadSociales.sociales = notaSociales;
-modalidadSociales.civica = notaCivica;
-modalidadSociales.politica = notaPolitica;
-modalidadSociales.sociologia = notaSociologia;
-
-console.log(`Notas de la Modalidad Sociales ${ Object.values(modalidadSociales)}`);
-console.log('Promedio: ', promedio(modalidadSociales));
-
-console.log('--------------------------------------------------');
-
-const modalidadNaturales = {biologia: 0, fisica: 0, quimica: 0, anatomia: 0};
-
-const notaBiologia: number = Number(prompt('Nota de Biologia: '));
-const notaFisica: number = Number(prompt('Nota de Fisica: '));
-const notaQuimica: number = Number(prompt('Nota de Quimica: '));
-const notaAnatomia: number = Number(prompt('Nota de Anatomia: '));
-
-modalidadNaturales.biologia = notaBiologia;
-modalidadNaturales.fisica = notaFisica;
-modalidadNaturales.quimica = notaQuimica;
-modalidadNaturales.anatomia = notaAnatomia;
-
-console.log(`Notas de la Modalidad Sociales ${ Object.values(modalidadNaturales)}`);
-console.log('Promedio: ', promedio(modalidadNaturales));
-
-console.log('--------------------------------------------------');
-
-// console.log(modalidadSociales);
-// console.log(modalidadNaturales);
-
-// console.log(promedio(modalidadSociales));
-// console.log(promedio(modalidadNaturales));
-*/
-
-const modalidadSociales = { sociales: 0, civica: 0, politica: 0, sociologia: 0 };
-const modalidadNaturales = { biologia: 0, fisica: 0, quimica: 0, anatomia: 0 };
-
-const juanPerez = new Alumnos('Juan', 'Perez', 42347925, '07/12/2010', 100, modalidadSociales, 10, { lengua: 'Juan Garcia', Biologia: 'Marcelo Bettini' });
-datosAlumnos.push(juanPerez);
-
-const marioGarcia = new Alumnos('Mario', 'Garcia', 40367121, '13/01/1009', 101, modalidadNaturales, 8, { Fisica: 'Tomas Cardenas' });
-datosAlumnos.push(marioGarcia);
-
-const joseMartinez = new Alumnos('Jose', 'Martinez', 44789098, '12/04/2011', 102, modalidadNaturales, 7, { quimica: 'Peto Menahen' });
-datosAlumnos.push(joseMartinez);
-
-//console.log(datosAlumnos);
 
 function promedio(object: {}) {
     const arrayNotas: Array<number> = Object.values(object);
@@ -77,24 +16,43 @@ function añadeAlumno() {
     const apellido = readlineSync.question('Ingrese apellido del alumno: ');
     const dni = readlineSync.question('Ingrese dni del alumno: ');
     const fechaNacimiento = readlineSync.question('Ingrese fecha de nacimiento del alumno, formato DD/MM/AAAA: ');
-    const modalidad = readlineSync.question('Elija la modalidad, Naturales - Sociales: ');
-    console.log(`Alumno añadido con Exito:
-    Nombre: ${nombre},
-    Apellido: ${apellido},
-    Dni: ${dni},
-    Fecha de Nacimiento: ${fechaNacimiento},
-    Modalidad: ${modalidad}`);
+    const matricula = 100
+    const modalidad = String(eligeModalidad());
+    const materias = añadeMaterias();
+    const promedioFinal = promedio(materias)
+    const profesores = añadeProfesores();
+
+    function añadeMaterias(): any {
+        if (modalidad === 'Naturales') return { biologia: 0, fisica: 0, quimica: 0, anatomia: 0 }; if (modalidad === 'Sociales') return { sociales: 0, civica: 0, politica: 0, sociologia: 0 };
+    }
+
+    function añadeProfesores(): any {
+        if (modalidad === 'Naturales') return { biologia: 'Manuel Barraza', fisica: 'Juan Manuel Martinez', quimica: 'Marcela Rojas', anatomia: 'Claudia Morales' };
+        if (modalidad === 'Sociales') return { sociales: 'Carlos Benitez', civica: 'Maria del Carmen Fernandez', politica: 'Roberto Mandraccio', sociologia: 'Cristina Del Curto' };
+    }
+
+    const alumno = new Alumnos(nombre, apellido, dni, fechaNacimiento, matricula, modalidad, materias, promedioFinal, profesores);
+
+    datosAlumnos.push(alumno);
+    console.log(`Alumno añadido con Exito`, alumno);
     menu();
 }
 
 function buscaAlumno() {
-    console.log('Se buscar alumno');
+    const apellido = readlineSync.question('Escriba el apellido del alumno que desea buscar: ');
+    let nombreEncontrado = datosAlumnos.find(element => element.apellido === apellido);
+    console.log(`Alumno encontrado`, nombreEncontrado);
     menu();
 }
 
 function eliminaAlumno() {
     console.log('Se elimina alumno');
     menu();
+}
+
+function listadoAlumnos() {
+    console.log(datosAlumnos);
+    menu()
 }
 
 function añadeProfesor() {
@@ -112,21 +70,29 @@ function eliminaProfesor() {
     menu();
 }
 
+function eligeModalidad(): any {
+    const modalidad = ['Naturales', 'Sociales'];
+    console.log('Seleccione la modalidad: ');
+    const seleccionModalidad = readlineSync.keyInSelect(modalidad);
+    if (modalidad[seleccionModalidad] === modalidad[0]) return modalidad[0];
+    if (modalidad[seleccionModalidad] === modalidad[1]) return modalidad[1];
+}
+
 function menu() {
-    const items = ['Anadir Alumno', 'Buscar Alumno', 'Eliminar Alumno', 'Anadir Profesor', 'Buscar Profesor', 'Eliminar Profesor'];
+    const items = ['Anadir Alumno', 'Buscar Alumno', 'Eliminar Alumno', 'Listado de Alumnos', 'Anadir Profesor', 'Buscar Profesor', 'Eliminar Profesor',];
     const seleccion = readlineSync.keyInSelect(items)
 
     if (items[seleccion] === items[0]) añadeAlumno();
     else if (items[seleccion] === items[1]) buscaAlumno();
     else if (items[seleccion] === items[2]) eliminaAlumno();
-    else if (items[seleccion] === items[3]) añadeProfesor();
-    else if (items[seleccion] === items[4]) buscaProfesor();
-    else if (items[seleccion] === items[4]) eliminaProfesor();
+    else if (items[seleccion] === items[3]) listadoAlumnos();
+    else if (items[seleccion] === items[4]) añadeProfesor();
+    else if (items[seleccion] === items[5]) buscaProfesor();
+    else if (items[seleccion] === items[6]) eliminaProfesor();
+
 }
 
 menu();
-
-
 
 //console.log(`Hola ${items[seleccion]}, como estas?`);
 
@@ -135,21 +101,3 @@ menu();
 
 // const nombre: number = Number(prompt('Escriba su Edad: '));
 // console.log(`Hola ${nombre}. Bienvenido!`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
